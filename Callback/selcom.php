90a6b9bf1854ef28b945c6a69768fdb17162ebe7
Emmanuel B. Muriaga <emmanuelmuriaga@gmail.com>
2023-05-16 08:04:39 +0300

    Created selcom callback file

---
+++ Callback/selcom.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * WHMCS Sample Payment Callback File
+ *
+ * This sample file demonstrates how a payment gateway callback should be
+ * handled within WHMCS.
+ *
+ * It demonstrates verifying that the payment gateway module is active,
+ * validating an Invoice ID, checking for the existence of a Transaction ID,
+ * Logging the Transaction for debugging and Adding Payment to an Invoice.
+ *
+ * For more information, please refer to the online documentation.
+ *
+ * @see https://developers.whmcs.com/payment-gateways/callbacks/
+ *
+ * @copyright Copyright (c) WHMCS Limited 2017
+ * @license http://www.whmcs.com/license/ WHMCS Eula
+ */
+// Require libraries needed for gateway module functions.
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+// Detect module name from filename.
